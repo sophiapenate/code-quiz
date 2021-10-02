2bb92a57ca5eb3569ec10459dd4e2c5531bbb955
Sophia Barrett <sophia@yoursummit.media>
2021-10-02 12:53:27 -0700

    add timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
-// store quiz questions as objects in quizQuestions array
+/*** QUIZ SETTINGS ***/
+
+// Set, in seconds, time given to finish quiz
+var timeGiven = 100;
+
+// Set, in seconds, time penalty for answering incorrectly
+var timePenalty = 10;
+
+// Set, in seconds, time rewarded for answering correctly
+var timeReward = 5;
+
+// Set message to display if player runs out of time
+var timesUpMessage = "Time's Up!"
+
+// Set message to display if player answeres all questions
+var answeredAllMessage = "Excellent! You answered every question!"
+
+// Set quiz questions
 var quizQuestions = [
     {
         question: "Question 1",
@@ -31,22 +48,44 @@ var quizQuestions = [
         answer: "True statement"
     }
 ];
+/*** END QUIZ SETTINGS ***/ 
 
 // Randomize quiz question order - push quizQuestions objects into a new array in a random order
 
-// store #quiz-content elements in variables
+// Set and display timer
+var timer = timeGiven;
+var timeRemainingEl = document.querySelector("#time-remaining");
+timeRemainingEl.textContent = timer;
+// Declare startTimer variable globally, so other functions can clear interval
+var startTimer;
+
+// Store #quiz-content elements in variables
 var promptTitleEl = document.querySelector("#prompt-title");
 var promptDetailsEl = document.querySelector("#prompt-details");
 var startBtn = document.querySelector("#start-btn");
 var optionsListEl = document.querySelector("#options");
 
-// create currentQuestion counter and store in variable
+// Set currentQuestion counter
 var currentQuestion = 0;
 
+// Start Quiz Function - To execute when start button is clicked
 var startQuiz = function() {
+    // start timer
+    startTimer = setInterval(function() {
+        timer--;
+        timeRemainingEl.textContent = timer;
+        // when timer runs out, stop timer and end quiz
+        if (timer === 0) {
+            clearInterval(startTimer);
+            endQuiz(timesUpMessage);
+        }
+    }, 1000)
+
+    // call first question
     nextQuestion();
 }
 
+// Answer Question Function - To execute when a question is answered
 var answerQuestion = function(event) {
     // check if a question button was clicked before proceeding
     var targetEl = event.target;
@@ -57,7 +96,15 @@ var answerQuestion = function(event) {
         if (pickedAnswer === correctAnswer) {
             console.log("Correct!");
         } else {
-            console.log("Sorry, that's incorrect.");
+            console.log("Sorry, that's incorrect. You lose " + timePenalty + " seconds");
+            // check if time remaining is greater than timePenalty
+            if (timer > timePenalty) {
+                timer -= timePenalty;
+            } else {
+                endQuiz(timesUpMessage);
+                return false;
+            }
+            
         }
         
         // update currentQuestion counter
@@ -67,11 +114,13 @@ var answerQuestion = function(event) {
         if (currentQuestion < quizQuestions.length) {
             nextQuestion();
         } else {
-            endQuiz();
+            // otherwise, end quiz
+            endQuiz(answeredAllMessage);
         }
     }
 }
 
+// Next Question Function - To execute at start of quiz and after each question is answered
 var nextQuestion = function() {
     // replace #quiz-content with current question content
     promptTitleEl.textContent = "Question " + (currentQuestion + 1);
@@ -87,9 +136,16 @@ var nextQuestion = function() {
     }
 }
 
-var endQuiz = function() {
-    promptTitleEl.textContent = "Game Over!";
-    promptDetailsEl.textContent = "Your score is ";
+// End Quiz Function - To execute when timer or questions run out
+var endQuiz = function(message) {
+    // stop timer
+    clearInterval(startTimer);
+
+    // set player's score equal to time remaining
+    var score = timer;
+
+    promptTitleEl.textContent = message;
+    promptDetailsEl.textContent = "Your score is " + score;
     optionsListEl.innerHTML = "";
 }
 
